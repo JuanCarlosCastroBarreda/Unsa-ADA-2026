2ecb91765a7db78c08e63941f22616083ae48846
JuanCarlosCastroBarreda <jcastrobar@unsa.edu.pe>
2026-09-05 22:11:45 -0500

    se agrego la razon

--- maximoarreglo/maximo_subarreglo.cpp
+++ maximoarreglo/maximo_subarreglo.cpp
@@ -64,8 +64,12 @@ int main(){
     int tam[] = {1000, 2000, 4000, 8000};
     int resultado_cubico, resultado_cuadratico, resultado_lineal;
 
+    double anterior_cubico = 0;
+    double anterior_cuadratico = 0;
+    double anterior_lineal = 0;
+
     cout<<"Tabla de resultados:"<<endl;
-    cout << "n\t\tSuma O(n^3)\tTiempo O(n^3)"<<"\t\tSuma O(n^2)\tTiempo O(n^2)"<<"\t\tSuma O(n)\tTiempo O(n)" << endl;
+    cout << "n\t\tSuma O(n^3)\tTiempo O(n^3)\tRazon"<<"\t\tSuma O(n^2)\tTiempo O(n^2)\tRazon"<<"\t\tSuma O(n)\tTiempo O(n)\tRazon" << endl;
     for(int i=0; i<4; i++){
         n = tam[i];
         L = new int[n];
@@ -76,9 +80,25 @@ int main(){
         double tiempo_n_cuadratico = medir_tiempo(n_cuadratico, resultado_cuadratico);
         double tiempo_n_lineal = medir_tiempo(n_lineal, resultado_lineal);
 
-        cout<<n<<"\t\t"<< resultado_cubico <<"\t\t"<< tiempo_n_cubico <<" ms\t\t"
-        << resultado_cuadratico <<"\t\t"<< tiempo_n_cuadratico <<" ms\t\t"
-        << resultado_lineal <<"\t\t"<< tiempo_n_lineal<<" ms"<<endl;
+        double razon_cubico = 0;
+        double razon_cuadratico = 0;
+        double razon_lineal = 0;
+
+        if(i > 0){
+            razon_cubico = tiempo_n_cubico / anterior_cubico;
+            razon_cuadratico = tiempo_n_cuadratico / anterior_cuadratico;
+            razon_lineal = tiempo_n_lineal / anterior_lineal;
+        }
+
+        cout<<n<<"\t\t"<< resultado_cubico <<"\t\t"<< tiempo_n_cubico <<" ms\t"<<razon_cubico<<"\t\t"
+        << resultado_cuadratico <<"\t\t"<< tiempo_n_cuadratico <<" ms\t"<<razon_cuadratico<<"\t\t"
+        << resultado_lineal <<"\t\t"<< tiempo_n_lineal<<" ms\t"<<razon_lineal<<endl;
+
+        anterior_cubico = tiempo_n_cubico;
+        anterior_cuadratico = tiempo_n_cuadratico;
+        anterior_lineal = tiempo_n_lineal;
+
+        delete[] L;
 
     }
     return 0;
